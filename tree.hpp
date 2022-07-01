b8d10eb2c399827455c9626934046933a26417ec
Dror Kabely <sdrork123@gmail.com>
2022-07-01 13:43:02 -0400

    [Tree] fixed list_tree_node template implementation
    Signed-off-by: Dror Kabely <sdrork123@gmail.com>

--- tree.hpp
+++ tree.hpp
@@ -4,12 +4,15 @@
 #include <iostream>
 #include <vector>
 
+#include "utils.hpp"
+
 using std::begin;
 using std::end;
 using std::distance;
+using std::size;
 
 class tree_node;
-typedef std::unique_ptr<tree_node> p_tree_node;
+typedef std::shared_ptr<tree_node> p_tree_node;
 
 class tree_node {
 protected:
@@ -29,11 +32,21 @@ public:
 template <class Elem> 
 class list_tree_node : public tree_node
 {
+    // iterators for container
 private:
     std::vector<Elem> m_elems;
 public:
     list_tree_node() : tree_node(0), m_elems({}){}
+    list_tree_node(Elem elem) : tree_node(0) {
+        m_elems.push_back(elem);
+    }
     list_tree_node(list_tree_node<Elem> const& cp) : tree_node(cp.get_line_number()), m_elems(cp.m_elems){}
+    list_tree_node(list_tree_node<Elem> const& base, 
+                   list_tree_node<Elem> const& ext) : list_tree_node(base) {
+
+        m_elems.reserve(size(m_elems) + distance(begin(ext.m_elems), end(ext.m_elems)));
+        m_elems.insert(end(m_elems), begin(ext.m_elems), end(ext.m_elems));
+    }
     ~list_tree_node() { m_elems.clear(); }
 
     virtual auto copy() -> p_tree_node {
@@ -41,23 +54,16 @@ public:
     }
 
     virtual auto dump(std::ostream& stream, int n) -> void {
-        for (auto const& elem : m_elems)
+        if (size(m_elems) == 0)
         {
-            stream << elem << std::endl;
+            stream << pad(n) << "nil" << std::endl;
         }
-        
-    }
-protected:
-    list_tree_node(list_tree_node<Elem> const& base, 
-                     list_tree_node<Elem> const& ext) : tree_node(base.get_line_number()), m_elems(base.m_elems){
-        m_elems.reserve(m_elems.size() + distance(begin(ext), end(ext)));
-        m_elems.insert(end(m_elems), begin(ext), end(ext));
+        else {
+            stream << pad(n) << "list" << std::endl;
+            for (Elem const& elem : m_elems)
+                elem->dump(stream, n+2);
+            stream << pad(n) << "endlist" << std::endl;
+        }
     }
 
-
-public:
-    static auto list_tree_node_append(list_tree_node<Elem> const& base,
-                                      list_tree_node<Elem> const& ext) -> list_tree_node* {
-        return new list_tree_node<Elem>(base, ext); // TODO: potential memory leak, switch to shared_ptr
-    }
 };
